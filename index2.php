9bb05d6d5e2d1875462236f13ce930b16390ae62
Arthur Wagenknecht <oarthurdev@gmail.com>
2021-02-26 09:18:32 -0300

    Including uniqid in file name, deleting the file after build the report.

--- index2.php
+++ index2.php
@@ -13,13 +13,14 @@ if(isset($_POST["hidden_div_html"]) && $_POST["hidden_div_html"] != '')
     $i=1;
     $result='';
     foreach ($tags as $tag) {
-        $file_name = 'images/google_chart'.$i.'.png';
+        $uniqId = uniqid();
+        $file_name = 'images/chart_'.$uniqId.'.png';
         
             $img_Src=$tag->getAttribute('src');
             
             file_put_contents($file_name, file_get_contents($img_Src));
             
-        $res= '<img src="images/google_chart'.$i.'.png">';
+        $res= '<img src="images/chart_'.$uniqId.'.png">';
         $result.=$res;
       $i++;
     }
@@ -32,5 +33,6 @@ if(isset($_POST["hidden_div_html"]) && $_POST["hidden_div_html"] != '')
     $mpdf->list_indent_first_level = 0; // 1 or 0 - whether to indent the first level of a list
     $mpdf->WriteHTML($result);
     $mpdf->Output();
+    unlink('images/chart_'.$uniqId.'.png');
 }
 ?>
